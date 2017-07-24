82e128bbf2fc2ee44d21ec21430728c032fd1f9e
Nhat Khanh Huy Tran <khanhhuy288@gmail.com>
2017-07-24 11:18:57 +0200

    solve 7 7kyu katas

---
+++ natoAlphabet.js
@@ -0,0 +1,20 @@
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  };
+  
+  return function(word) {
+    return word.toUpperCase().split('').map(x => letters[x]).join(' ');
+  };
+})(); 
+
+console.log(nato('hi')); // --> 'Hotel India'
+console.log(nato('abc')); // --> 'Alpha Bravo Charlie'
