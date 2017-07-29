a044e3feb173003ecf0d055971bfde7eb1419911
Nhat Khanh Huy Tran <khanhhuy288@gmail.com>
2017-07-29 23:59:21 +0200

    solve 6 7kyu katas

--- accum.js
+++ accum.js
@@ -2,7 +2,7 @@ function accum(s) {
   return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 }
     
-console.log(accum("ZpglnRxqenU")); 			// "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("ZpglnRxqenU")) 			// "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
 console.log(accum("NyffsGeyylB")); 			// "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
 console.log(accum("MjtkuBovqrU")); 			// "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
 console.log(accum("EvidjUnokmM")); 			// "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
